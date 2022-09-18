c0576d51b787be3add4e05fca2bb16c9c4e90013
Trehub Anatolii <tridfoto@gmail.com>
2022-09-18 21:40:00 +0300

    upd mobile, add select views

--- jsscripts.js
+++ jsscripts.js
@@ -1,2 +1,16 @@
 
 document.querySelector('.form-range').oninput = (event) => document.querySelector('.inp-sp').innerHTML = event.target.value;
+
+document.querySelector('.mob-menu').onclick = () => {
+
+    document.querySelector('.left-menu').classList.remove("hide-mobile");
+    document.querySelector('.left-menu').classList.add("dropdown-content");
+}
+
+document.querySelector('.butok').onclick = () => {
+    document.querySelector('.left-menu').classList.add("hide-mobile");
+    document.querySelector('.left-menu').classList.remove("dropdown-content");
+}
+
+document.querySelector('.views-small').onclick = (event) =>
+    document.querySelector('.view-large').src = event.target.src;
